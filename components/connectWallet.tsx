b347739a394fe75a2fb9042b9157068d40b40f4c
macadmin <macadmin@MacBook-Pro.attlocal.net>
2022-08-18 16:57:45 -0700

    add connectWallet component

---
+++ components/connectWallet.tsx
@@ -0,0 +1,34 @@
+import { useEffect, useState } from "react";
+import { BrowserWallet } from "@martifylabs/mesh";
+import type { Wallet } from "@martifylabs/mesh";
+import useWallet from "../contexts/wallet";
+
+
+export default function connectWallet() {
+    const [availableWallets, setAvailableWallets] = useState<Wallet [] | undefined>(undefined);
+    const { walletNameConnected, connecting, connectWallet, walletConnected } = useWallet();
+
+    useEffect(() => {
+        async function init () {
+            setAvailableWallets(BrowserWallet.getInstalledWallets());
+        }
+        init ();
+    }, []);
+    return (
+        <>
+            {availableWallets 
+                ? availableWallets.length == 0 
+                    ? "No wallets found"
+                    : availableWallets.map((wallet, i) => (
+                        <button
+                            key={i}
+                            onClick={() => connectWallet(wallet.name)}
+                            disabled= { walletConnected || connecting || walletNameConnected
+                             == wallet.name }>
+
+                        </button>
+                    ))
+            }
+        </>
+    )
+}
