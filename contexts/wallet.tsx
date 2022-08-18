e22ad7d45a2ab2f04b14b29ad3a51b7bdd6e6934
macadmin <macadmin@MacBook-Pro.attlocal.net>
2022-08-18 16:57:21 -0700

    import BrowserWallet instead of WalletService

--- contexts/wallet.tsx
+++ contexts/wallet.tsx
@@ -5,10 +5,10 @@ import React, {
     useMemo, 
     ReactNode, 
 } from "react";
-import { WalletService } from "@martifylabs/mesh";
+import { BrowserWallet } from '@martifylabs/mesh';
 
 const WalletContext = createContext({
-    wallet: {} as WalletService,
+    wallet: {} as BrowserWallet,
     connecting: false,
     walletNameConnected: "",
     walletConnected: false,
@@ -16,14 +16,14 @@ const WalletContext = createContext({
 });
 
 export const WalletProvider = ({ children }: { children: ReactNode }) => {
-    const [wallet, setWallet] = useState<WalletService>({} as WalletService);
+    const [wallet, setWallet] = useState<BrowserWallet>({} as BrowserWallet);
     const [walletConnected, setWalletConnected] = useState<boolean>(false);
     const [connecting, setConnecting] = useState<boolean>(false);
     const [walletNameConnected, setWalletNameConnected] = useState<string>("");
 
     const connectWallet = async (walletName: string) => {
         setConnecting(true);
-        const _wallet = await WalletService.enable(walletName);
+        const _wallet = await BrowserWallet.enable(walletName);
         if (_wallet) {
             setWallet(_wallet);
             setWalletNameConnected(walletName);
