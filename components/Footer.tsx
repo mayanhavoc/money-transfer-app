56cf2bf34cdb04070a40c664928e34ce4825ff05
macadmin <macadmin@MacBook-Pro.attlocal.net>
2022-08-19 15:50:12 -0700

    Replaced lorem with wallet connection info

--- components/Footer.tsx
+++ components/Footer.tsx
@@ -1,16 +1,31 @@
 import { useEffect, useState } from 'react';
 import Link from 'next/link';
-import { Flex, Spacer, Text } from '@chakra-ui/react';
-// import useWallet from '../components/wallet';
-import connectWallet from './connectWallet';
-import { stringify } from 'querystring';
-import { printLocation } from '../node_modules/graphql/index';
-import { PollingWatchKind } from '../node_modules/typescript/lib/typescript';
+import { Flex, Spacer, Text, Box } from '@chakra-ui/react';
+import useWallet from '../contexts/wallet';
+import ConnectWallet from './connectWallet';
+
 
 export default function Footer() {
+    const { connecting, connectWallet, walletConnected, connectedAddress, currentNetwork, walletNameConnected } = useWallet();
+    const [footerColor, setFooterColor] = useState('green.200')
+
+    useEffect(()=> {
+        if(walletConnected){
+            setFooterColor('purple.200')
+        }
+    }, [walletConnected])
+
     return (
         <Flex pos="fixed" bottom="0" direction="row" w="100%" p="5" bg="pink.500">
-            <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Ut, consequatur?</p>
+            { walletConnected ? (
+                <Text>Connected to {currentNetwork} on {walletNameConnected}</Text>
+            ) : (
+                <Text>Connect a wallet</Text>
+            )}
+            <Spacer/>
+            {/* <ConnectWallet /> */}
+            <Spacer/>
+            {connectedAddress}
         </Flex>
     )
 }
