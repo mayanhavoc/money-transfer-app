b4ff20736c5c317261d87b4648b62dcbb7b4596d
macadmin <macadmin@MacBook-Pro.attlocal.net>
2022-08-19 10:23:43 -0700

    add apollo client

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -2,6 +2,8 @@ import "../styles/globals.css";
 import type { AppProps } from "next/app";
 import { WalletProvider } from "../contexts/wallet";
 import { ChakraProvider } from "@chakra-ui/react";
+import { ApolloProvider } from "@apollo/client";
+import client from "../apollo-client";
 
 import Navbar from "../components/Navbar";
 import Footer from "../components/Footer";
