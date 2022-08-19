2d4d0d30037d3cda5b9d88a6e40f19a1e9dd7031
macadmin <macadmin@MacBook-Pro.attlocal.net>
2022-08-19 10:16:37 -0700

    build Footer component

--- components/Footer.tsx
+++ components/Footer.tsx
@@ -1,1 +1,16 @@
 import { useEffect, useState } from 'react';
+import Link from 'next/link';
+import { Flex, Spacer, Text } from '@chakra-ui/react';
+// import useWallet from '../components/wallet';
+import connectWallet from './connectWallet';
+import { stringify } from 'querystring';
+import { printLocation } from '../node_modules/graphql/index';
+import { PollingWatchKind } from '../node_modules/typescript/lib/typescript';
+
+export default function Footer() {
+    return (
+        <Flex pos="fixed" bottom="0" direction="row" w="100%" p="5" bg="pink.500">
+            <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Ut, consequatur?</p>
+        </Flex>
+    )
+}
