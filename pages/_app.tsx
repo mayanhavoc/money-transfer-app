5279fc96ba9b8fe4923d7efaff7b0429b011d51d
macadmin <macadmin@MacBook-Pro.attlocal.net>
2022-08-19 10:08:01 -0700

    add Chakra UI

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,12 +1,18 @@
 import "../styles/globals.css";
 import type { AppProps } from "next/app";
 import { WalletProvider } from "../contexts/wallet";
+import { ChakraProvider } from "@chakra-ui/react";
+
+import Navbar from "../components/Navbar";
 
 function MyApp({ Component, pageProps }: AppProps) {
     return (
-        <WalletProvider>
-            <Component {...pageProps} />
-        </WalletProvider>
+        <ChakraProvider>
+            <WalletProvider>
+                <Navbar />
+                <Component {...pageProps} />
+            </WalletProvider>
+        </ChakraProvider>
     );
 }
 
