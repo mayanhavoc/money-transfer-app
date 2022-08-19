0f39002e422c1b606764ec50246a61f3bd2a9927
macadmin <macadmin@MacBook-Pro.attlocal.net>
2022-08-19 10:53:09 -0700

    add wallet address

--- contexts/wallet.tsx
+++ contexts/wallet.tsx
@@ -1,18 +1,19 @@
 import React, {
-    createContext,
-    useState,
-    useContext, 
-    useMemo, 
-    ReactNode, 
+      createContext
+    , useState
+    , useContext
+    , useMemo
+    , ReactNode, 
 } from "react";
 import { BrowserWallet } from '@martifylabs/mesh';
 
 const WalletContext = createContext({
-    wallet: {} as BrowserWallet,
-    connecting: false,
-    walletNameConnected: "",
-    walletConnected: false,
-    connectWallet: async (walletName: string) => {},
+    wallet: {} as BrowserWallet
+    , connecting: false
+    , walletNameConnected: ""
+    , walletConnected: false
+    , connectWallet: async (walletName: string) => {}
+    , connectedAddress: "",
 });
 
 export const WalletProvider = ({ children }: { children: ReactNode }) => {
@@ -20,25 +21,29 @@ export const WalletProvider = ({ children }: { children: ReactNode }) => {
     const [walletConnected, setWalletConnected] = useState<boolean>(false);
     const [connecting, setConnecting] = useState<boolean>(false);
     const [walletNameConnected, setWalletNameConnected] = useState<string>("");
+    const [connectedAddress, setConnectedAddress] = useState<string>("");
 
     const connectWallet = async (walletName: string) => {
         setConnecting(true);
         const _wallet = await BrowserWallet.enable(walletName);
+        const _address = await _wallet.getUsedAddresses();
         if (_wallet) {
             setWallet(_wallet);
             setWalletNameConnected(walletName);
             setWalletConnected(true);
+            setConnectedAddress(_address[0]);
         }
         setConnecting(false);
     };
 
     const memoedValue = useMemo(
         () => ({
-            wallet, 
-            connecting,
-            walletNameConnected,
-            walletConnected,
-            connectWallet,
+              wallet
+            , connecting
+            , walletNameConnected
+            , walletConnected
+            , connectWallet
+            , connectedAddress,
         }),
         [wallet,walletConnected, connecting, walletNameConnected]
     );
